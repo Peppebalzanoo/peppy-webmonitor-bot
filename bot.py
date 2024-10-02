16ccd3068044ce06d112728c3b2e4efc5e58c1c3
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2024-10-02 22:05:21 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from telegram import Update
+from dotenv import load_dotenv
+#?# Per usare MARKDOWN/HTML nella formattazione
+from telegram.constants import ParseMode
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+#? Load .env variables and override system's variables
+load_dotenv(override=True)
+
+#? Per loggare in console informazioni e status dell'applicazione
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+#? Utilizziamo async per definire funzioni asincrone
+#? Utilizziamo await per chiamare un metodo in modo asincrono
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hi <b>{update.message.from_user.username}</b>, let me introduce myself...\nmy name is <b>Peppy</b> and I am <i>website monitoring bot</i>!", parse_mode=ParseMode.HTML)
+
+
+# async def handle_message_all(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     print("handle_message_all()")
+#     await update.message.reply_text(f"Thanks {update.message.from_user.username} for writing me!")
+#     with open(f"chats_log/{update.message.chat_id}_{update.message.from_user.username}.log", "a") as file:
+#         file.write(f"[Bot]: {None}\n")
+#         file.write(f"[{update.message.from_user.username}]: {update.message.text}\n")
+
+async def handle_message_regex(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print("handle_message_regex()")
+    msg = f"Grazie {update.message.from_user.username} per avermi scritto! Ti saluto volentieri!"
+    await update.message.reply_text(msg)
+    with open(f"chats_log/{update.message.chat_id}_{update.message.from_user.username}.log", "a") as file:
+        file.write(f"[@Bot]: {msg}\n[@{update.message.from_user.username}]: {update.message.text}\n\n")
+
+def main() -> None:
+
+    app = Application.builder().token(os.getenv("TOKEN_API")).build()
+
+    #? Handler del comando start
+    app.add_handler(CommandHandler("start", start))
+
+    #? Handler dei messaggi in chat: filters.ALL per attivare su tutto
+    # app.add_handler(MessageHandler(filters.ALL, handle_message_all))
+
+    #? Handler dei messaggi in chat: filters.Regex per attivare sulle Regex
+    #? Case-Insensitive: (?i)
+    app.add_handler(MessageHandler(filters.Regex(r"(?i)saluto"), handle_message_regex))
+
+    #? Bot in ascolto (pooling)
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
---
+++ peppy_v0.0.1.py
@@ -0,0 +1,65 @@
+import os
+import asyncio
+import requests
+import validators
+import logging
+from dotenv import load_dotenv
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+#? Per loggare in console informazioni e status dell'applicazione
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+#? Load .env variables and override system's variables
+load_dotenv(override=True)
+
+def is_valid_url(url):
+    return validators.url(url)
+
+async def validation_url(update, url):
+    if len(url) <= 0:
+        await update.message.reply_text("Sorry, empty URL was passed!")
+    else:
+        validation = is_valid_url(url)
+        if validation:
+            await update.message.reply_text("Ok, added!")
+        else:
+            await update.message.reply_text("Sorry, invalid URL was passed!")
+
+
+async def start(update, context):
+    print("Starting bot...")
+    await update.message.reply_text("Hi, I'm Peppy WebMonitor Bot!")
+    print("Starting done!")
+
+async def routine(update, url):
+     while True:
+            # Make the HTTP request to the website
+            response = requests.get(url)
+            previous_content = ""
+            if os.path.isfile("content.txt"):
+                with open("content.txt", "r", encoding="utf-8") as file:
+                    previous_content = file.read()
+                    if response.text != previous_content:
+                        print("Website content has changed!")
+                        await update.message.reply_text("Hi, website content has changed!")
+
+                with open("content.txt", "w", encoding="utf-8") as file:
+                    # Update the saved content
+                     file.write(response.text)
+
+            # Wait for the specified interval before the next check
+            await asyncio.sleep(60)
+
+async def add(update, context):
+    url = update.message.text.replace("/add", "").replace(" ", "")
+    print(url)
+    await validation_url(update, url)
+    await routine(update, url)
+
+
+app = Application.builder().token(os.getenv("TOKEN_API")).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("add", add))
+app.run_polling()
