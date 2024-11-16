a808cd0e83c5ce44e2a2c0c596f3adcadd125e17
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2024-11-16 22:45:58 +0100

    1. Fix bug about url length when showing it on unfollow_command() buttons.
    2. Modification of show_list_command().
    
    3. Translated the comments into English
    
    4. Added comments to the code
    
    5. Added time constant to check if urls have changed.

--- peppybot.py
+++ peppybot.py
@@ -11,25 +11,27 @@ import logging
 import asyncio
 import os
 
-# Per loggare in console informazioni e status dell'applicazione
+# To log into console information and status of application
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
-# key: username, value: set("url1",...,"urlN")
+# key: username; value: set("url1",...,"urlN")
 user_url_dict = {}
-# key: (username, url), value: task_id
-task_dict = {}
 
+# key: (username, url); value: task_id
+task_dict = {}
 
 # Load .env (environment) and override system's variables
 load_dotenv(override=True)
 
-# Recupero environment-variables from .env
+
+# Retrieve environment-variables from .env
 def load_token_api() -> str:
     try:
         return os.getenv("TOKEN_API")
     except ValueError:
         logging.error("Invalid TOKEN_API in .env file")
         return ""
+
 
 def load_state_request_url() -> int:
     try:
@@ -38,12 +40,14 @@ def load_state_request_url() -> int:
         logging.error("Invalid STATE_REQUEST_URL in .env file")
         return -1
 
+
 def load_allowed_ids() -> list[int]:
     try:
         return [int(user_id) for user_id in os.getenv("ALLOWED_IDS").replace(" ", "").split(",")]
     except ValueError:
         logging.error("Invalid ALLOWED_IDS in .env file")
         return []
+
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = f"Hello <b>{update.message.from_user.username}</b>, it's a pleasure to meet you! I am @PeppyWebMonitorBot.\n" \
@@ -58,27 +62,31 @@ async def show_help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) 
            "/start - restart the bot\n" \
            "/follow - follow a URL\n" \
            "/unfollow - unfollow a URL\n" \
+           "/stop - stop tracking of all URLs\n" \
+           "/cancel - cancel command\n" \
            "/list - display followed URLs\n" \
            "/help - show this list of commands\n"
     await update.message.reply_text(f"Hi <b>{update.message.from_user.username}</b>, you are in /help command!\n"
                                     f"{text}", parse_mode=ParseMode.HTML)
 
+
 async def stop_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     username = update.message.from_user.username
-    # Cancella tutti i task per l'utente
-    curr_set_urls = user_url_dict.get(username, set())
+    curr_set_urls = user_url_dict.get(username, set())  # Clear all tasks for the user.
 
     for url in curr_set_urls:
         task: asyncio.Task = task_dict.pop((username, url))
         if task:
-            task.cancel()  # Cancella il task
+            # Delete the task
+            task.cancel()
 
-    # Rimuovi l'utente dalle strutture di memorizzazione
+     # Remove user
     user_url_dict.pop(username, None)
 
     logging.debug(print(user_url_dict))
     logging.debug(print(task_dict))
     await update.message.reply_text("All monitoring has been stopped. You can reactivate me using /start.")
+
 
 async def show_list_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     curr_set_urls: set = user_url_dict.get(update.message.from_user.username)
@@ -90,6 +98,7 @@ async def show_list_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(text, disable_web_page_preview=True)
     else:
         await update.message.reply_text("You are not currently following any URLs.")
+
 
 async def fetch_url_content(url: str) -> str:
     try:
@@ -104,9 +113,11 @@ async def fetch_url_content(url: str) -> str:
         logging.error(f"Request error for {url}: {e}")
         return ""
 
+
 async def check_for_changes(prev_content: str, curr_content: str, url: str, update: Update):
     if prev_content and curr_content and prev_content != curr_content:
         await update.message.reply_text(f"Content changed for {url}.")
+
 
 async def trak_routine(update: Update, url) -> None:
     prev_content = ""
@@ -115,26 +126,33 @@ async def trak_routine(update: Update, url) -> None:
             curr_content = await fetch_url_content(url)
             await check_for_changes(prev_content, curr_content, url, update)
             prev_content = curr_content
-            await asyncio.sleep(60)
+            await asyncio.sleep(TIME)
     except CancelledError:
         await update.message.reply_text(f"Monitoring of {url} stopped.")
 
+
 async def start_task(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
-        url: str = context.user_data.get("URL")  # Recupero l'url dal contesto
-        curr_user_set: set = user_url_dict.get(update.message.from_user.username)
+        # Retrieve the url from the context.
+        url: str = context.user_data.get("URL")
 
-        if curr_user_set is None:
-            curr_user_set = set()
+        # If the set of urls for the user does not yet exist, create it
+        curr_user_set: set = user_url_dict.get(update.message.from_user.username) or set()
 
         if url not in curr_user_set:
+            # Add url to user's url set
             curr_user_set.add(url)
-            await update.message.reply_text("URL added successfully!")
-            user_url_dict[update.message.from_user.username] = curr_user_set
 
+            # Map the user with the set of his urls
+            user_url_dict[update.message.from_user.username] = curr_user_set
             logging.debug(print(user_url_dict))
+
+            await update.message.reply_text("URL added successfully!")
+
             curr_task: asyncio.Task = create_task(trak_routine(update, url))
             logging.debug(print(curr_task))
+
+            # Map the user to the current task
             task_dict[(update.message.from_user.username, url)] = curr_task
         else:
             await update.message.reply_text("This URL has already been added.")
@@ -142,29 +160,36 @@ async def start_task(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
         await update.message.reply_text("An error occurred while starting the task.")
         logging.error(f"Error in start_task: {e}")
 
+
 async def validate_url(update: Update, url):
     if not validators.url(url):
         logging.warning(f"Invalid URL provided by user {update.message.from_user.username}: {url}")
         return False
     return True
 
+
 async def state_request_url(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     url: str = update.message.text.replace("/follow", "").replace(" ", "")
 
     if len(url) <= 0:
         await update.message.reply_text("The provided URL is empty. Please enter a valid URL:")
-        return STATE_REQUEST_URL  #? Rimango in questo stato per attendere l'url da seguire
-
-    # Url without query string
-    url: str = urljoin(url, urlparse(url).path)
-
-    if await validate_url(update, url):
-        context.user_data["URL"] = url  #? Salvo l'url nel contesto
-        await start_task(update, context)
-        return ConversationHandler.END  #? Se l'url è stato aggiunto o meno, chiudiamo la conversazione
+        # Return to this state to wait for the url to follow
+        return STATE_REQUEST_URL
     else:
-        await update.message.reply_text("The provided URL is invalid. Please try again or use /cancel.")
-        return STATE_REQUEST_URL  #? Rimango in questo stato per attendere l'url da seguire
+        # Url without query string
+        url: str = urljoin(url, urlparse(url).path)
+
+        if await validate_url(update, url):
+
+            # Save the url in context
+            context.user_data["URL"] = url
+
+            await start_task(update, context)
+            return ConversationHandler.END
+        else:
+            await update.message.reply_text("The provided URL is invalid. Please try again or use /cancel.")
+            # Return to this state to wait for the url to follow
+            return STATE_REQUEST_URL
 
 
 async def follow_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -176,8 +201,9 @@ async def follow_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             return ConversationHandler.END
 
     await update.message.reply_text("Please enter the URL you wish to follow:")
-    return STATE_REQUEST_URL  #? Passo in questo stato per attendere l'url da seguire
 
+    # Return to this state to wait for the url to follow
+    return STATE_REQUEST_URL
 
 
 async def cancel_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -193,6 +219,10 @@ async def unfollow_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -
     else:
         choise = []
         for idx, url in enumerate(curr_set_urls):
+            # If the url is larger than 64 bytes, it is too large to be printed as a button
+            if len(url.encode("utf-8")) > 64:
+                diff = len(url.encode("utf-8")) - 64
+                url = url[:-diff]
             choise.append([InlineKeyboardButton(f"{idx + 1}. {url}", callback_data=url)])
         choise_markup = InlineKeyboardMarkup(choise)
         await update.message.reply_text(f"Please select the URL you wish to unfollow:", reply_markup=choise_markup)
@@ -211,20 +241,19 @@ async def unfollow_callback(update: Update, context: CallbackContext) -> None:
         await asyncio.sleep(0.3)
         logging.debug(print(f"Task for {curr_option} has been terminated! {task_to_remove.done()}"))
 
-    # Per eliminare l'option selezionata dalla funzione
+    # To delete the selected option from the function
     await update.callback_query.delete_message()
 
-
-MAX_TASKS_PER_USER = 5
+TIME = 60 #Seconds to check whether urls have changed
+MAX_TASKS_PER_USER = 5 #Maximum number of tasks per user
 ALLOWED_IDS = load_allowed_ids()
 TOKEN_API = load_token_api()
 STATE_REQUEST_URL = load_state_request_url()
 
-def main() -> None:
 
+def main() -> None:
     conv_handler = ConversationHandler(entry_points=[CommandHandler("follow", follow_command)],
-                                       states={STATE_REQUEST_URL: [MessageHandler(filters.TEXT & ~filters.COMMAND, state_request_url),
-                                                                   CommandHandler("cancel", cancel_command)], },
+                                       states={STATE_REQUEST_URL: [MessageHandler(filters.TEXT & ~filters.COMMAND, state_request_url), CommandHandler("cancel", cancel_command)], },
                                        fallbacks=[CommandHandler("start", start_command)], )
 
     app = Application.builder().token(TOKEN_API).build()
@@ -233,8 +262,10 @@ def main() -> None:
     app.add_handler(CommandHandler("stop", stop_command, filters=filters.User(ALLOWED_IDS)))
     app.add_handler(CommandHandler("follow", follow_command, filters=filters.User(ALLOWED_IDS)))
     app.add_handler(CommandHandler("cancel", cancel_command, filters=filters.User(ALLOWED_IDS)))
+
     app.add_handler(CommandHandler("unfollow", unfollow_command, filters=filters.User(ALLOWED_IDS)))
     app.add_handler(CallbackQueryHandler(unfollow_callback))
+
     app.add_handler(CommandHandler("list", show_list_command, filters=filters.User(ALLOWED_IDS)))
     app.add_handler(CommandHandler("help", show_help_command, filters=filters.User(ALLOWED_IDS)))
 
